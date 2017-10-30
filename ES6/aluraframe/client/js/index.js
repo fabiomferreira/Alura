340ad85bcadfd10ba4e9f203ccd6fa248c29a572
Fabio Ferreira <fabiomataferreira@gmail.com>
2017-10-30 15:10:10 -0400

    index js comecado

---
+++ ES6/aluraframe/client/js/index.js
@@ -0,0 +1,9 @@
+var campos = [
+    document.querySelector('#data'),
+    document.querySelector('#quantidade'),
+    document.querySelector('#valor')
+];
+
+document.querySelector('.form').addEventListener('submit', function(event){
+    alert('oi');
+});
